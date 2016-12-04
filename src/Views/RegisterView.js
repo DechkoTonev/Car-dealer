d8e854102745e56e9ab7e8c2a16ccda9e7a8ad43
AndrianStoikov <sandi6842@gmail.com>
2016-12-04 17:06:18 +0200

    Add files via upload

--- src/Views/RegisterView.js
+++ src/Views/RegisterView.js
@@ -3,23 +3,6 @@ import React, { Component } from 'react';
 export default class RegisterView extends Component {
     render() {
         return (
-            //<form className="register-form" onSubmit={this.submitForm.bind(this)}>
-            //    <h1>Register</h1>
-            //    <label>
-            //        <div>Username:</div>
-            //        <input type="text" name="username" required
-            //               ref={e => this.usernameField = e} />
-            //    </label>
-            //    <label>
-            //        <div>Password:</div>
-            //        <input type="password" name="password" required
-            //               ref={e => this.passwordField = e} />
-            //    </label>
-            //    <div>
-            //        <input type="submit" value="Register" />
-            //    </div>
-            //</form>
-
             <section className="section-divider textdivider divider6">
                 <div className="container">
                     <div className="row">
@@ -45,6 +28,14 @@ export default class RegisterView extends Component {
                                         </div>
                                     </div>
                                     <div className="form-group">
+                                        <label className="control-label"></label>
+                                        <div className="">
+                                            <input type="text" className="form-control"
+                                                   id="inputName" placeholder="Enter your email"
+                                                   ref={e => this.emailField = e} required/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
                                         <div className="">
                                             <button type="submit" className="btn btn-success">Register</button>
                                         </div>
@@ -61,6 +52,8 @@ export default class RegisterView extends Component {
     submitForm(event) {
         event.preventDefault();
         this.props.onsubmit(
-            this.usernameField.value, this.passwordField.value);
+            this.usernameField.value,
+            this.passwordField.value,
+            this.emailField.value);
     }
 }
